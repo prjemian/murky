99e4b3fe48cc2be6431b3bcc7c4917f597ce6987
Pete R Jemian <prjemian@gmail.com>
2024-02-10 14:45:47 -0600

    MNT #20 update copyright

--- murky/create_release_notes.py
+++ murky/create_release_notes.py
@@ -309,7 +309,7 @@ if __name__ == "__main__":
 # -----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
 # :email:     prjemian@gmail.com
-# :copyright: (c) 2014-2022, Pete R. Jemian
+# :copyright: (c) 2014-2024, Pete R. Jemian
 #
 # Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
 #
--- murky/murky_create.sh
+++ murky/murky_create.sh
@@ -151,7 +151,7 @@ echo "    conda activate ${environment}"
 # -----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
 # :email:     prjemian@gmail.com
-# :copyright: (c) 2014-2022, Pete R. Jemian
+# :copyright: (c) 2014-2024, Pete R. Jemian
 #
 # Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
 #
