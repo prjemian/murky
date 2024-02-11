f1e3e5a99def24f2c79f296381da98343d9cc1cd
Pete R Jemian <prjemian@gmail.com>
2024-02-10 20:17:31 -0600

    TST #19

--- murky/tests/test_crn.py
+++ murky/tests/test_crn.py
@@ -1,9 +1,5 @@
-from .. import create_release_notes
-import datetime
+"""Test the create_release_notes module."""
 
 
-def test_str2time():
-    given = "Tue, 20 Dec 2016 17:35:40 GMT"
-    result = create_release_notes.str2time(given)
-    assert isinstance(result, datetime.datetime)
-    assert str(result) == "2016-12-20 17:35:40"
+def test_todo():
+    assert True
