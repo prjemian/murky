1d4b633af56bac70032ee47c94766e0992cd685c
Pete R Jemian <prjemian@gmail.com>
2024-02-19 14:59:23 -0600

    DOC finish the move

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import json
 import pathlib
 import sys
 import tomllib
@@ -47,18 +48,32 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# https://pydata-sphinx-theme.readthedocs.io/en/latest/user_guide/version-dropdown.html
 #
 # html_theme = "alabaster"
-html_theme = "pydata_sphinx_theme"
-html_static_path = ["_static"]
-
 html_baseurl = ""
+html_static_path = ["_static"]
+html_theme = "pydata_sphinx_theme"
+
+# fmt: off
+switcher_file = "_static/switcher.json"
+switcher_json_url = (
+    "https://raw.githubusercontent.com/"
+    f"{gh_org}/{project}/"
+    "main/docs/source"
+    f"/{switcher_file}"
+)
+with open(switcher_file) as fp:
+    switcher_version_list = [
+        v["version"]  # to match with ``release`` (above)
+        for v in json.load(fp)
+    ]
+# fmt: on
 html_theme_options = {
-    # https://pydata-sphinx-theme.readthedocs.io/en/latest/user_guide/version-dropdown.html
-    "switcher": {
-        "json_url": "https://raw.githubusercontent.com/prjemian/murky/main/docs/switcher.json",
-        "version_match": version_match,
-    },
     "navbar_start": ["navbar-logo", "version-switcher"],
+    "switcher": {
+        "json_url": switcher_json_url,
+        "version_match": release if release in switcher_version_list else "dev"
+    },
     "navigation_with_keys": True,
 }
